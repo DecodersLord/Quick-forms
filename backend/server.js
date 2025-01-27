38e5b5a38d49b9e16a7e4d44f1447fb612ae9a04
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2025-01-27 16:16:45 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import fieldsRoutes from "./routes/fields.route.js";
 import formsRoutes from "./routes/forms.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/fields", fieldsRoutes);
 app.use("/api/forms", formsRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => {
     connectToMongoDB();
